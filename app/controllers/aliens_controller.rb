203432acc006a06500f8a1f9cd2b0e2e1a7661c9
AlanHausinger <alanh621@gmail.com>
2021-01-11 14:31:30 -0800

    finished adding endpoints and validations

--- app/controllers/aliens_controller.rb
+++ app/controllers/aliens_controller.rb
@@ -1,2 +1,27 @@
 class AliensController < ApplicationController
-end
+    def index
+        aliens = Alien.all 
+        render json: aliens 
+    end
+  
+    def create
+        alien = Alien.create(alien_params)
+        if alien.valid? 
+        render json: alien
+        else
+            render json: alien.errors, status: :unprocessable_entity
+    end
+    end
+    private
+    def alien_params
+        params.require(:alien).permit(:name, :distance, :planet, :img)
+    end
+  
+    def update
+    end
+  
+    def destroy
+    end
+  
+  end
+
--- app/models/alien.rb
+++ app/models/alien.rb
@@ -1,2 +1,5 @@
 class Alien < ApplicationRecord
+    validates :name, presence: true
+    validates :distance, presence: true
+    validates :planet, presence: true
 end
